5f5a91c468df9d2d4e14d2d90fff3d45e67f0ab8
FranDev <olmelofran@gmail.com>
2022-05-07 15:28:29 +0200

    new version and rcon module

--- app/modules/commands.py
+++ app/modules/commands.py
@@ -2,11 +2,13 @@ import discord
 from discord.ext.commands import Bot, Cog, command
 import aiohttp
 import random
+from os.path import join, dirname
 
 
 class Commands(Cog):
     def __init__(self, bot: Bot):
         self.bot = bot
+        self.whitelist_users = join(dirname(__file__), '../configs/whitelisted.json')
 
     # Reddit meme generator
     @command(pass_context=True)
@@ -22,6 +24,20 @@ class Commands(Cog):
     @command(name='eraser')
     async def purge(self, ctx, amount=200):
         await ctx.channel.purge(limit=amount)
+
+    @command(name='announce')
+    async def announce(self, ctx):
+        pass
+
+    @command(name='add_whitelist')
+    async def add_whitelist(self, ctx, user: discord.User):
+        # Add discord role for whitelist and rcon connection to addit
+        pass
+
+    @command(name='remove_whitelist')
+    async def remove_whitelist(self, ctx, user: discord.User):
+        # Add discord role for whitelist and rcon connection to addit
+        pass
 
 
 def setup(bot):
